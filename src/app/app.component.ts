f3e28c6dddfb3a6375f63e3180776d9548b50f4f
Dragomir Mildov <drashoo@abv.bg>
2018-10-31 07:31:29 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-reactive-forms';
+  rForm: FormGroup;
+  post:any;
+  description:string;
+  name: string;
+  titleAlert: string = 'This field is required';
+
+  constructor(private fb: FormBuilder) {
+    this.rForm = fb.group({
+      name: [null, Validators.required],
+      description: [
+        null,
+        Validators.compose([Validators.required, 
+        Validators.minLength(30),
+        Validators.maxLength(500)
+      ])],
+      validate: ''
+    })
+  }
+
+  addPost(post) {
+    this.description = post.description;
+    this.name = post.name;
+  }
+
 }
